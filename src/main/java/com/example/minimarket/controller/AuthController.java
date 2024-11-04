682eed9ae108cbd68b53c1d95c164093893dec00
gudtj <gudtjq2459@gmail.com>
2024-11-04 18:41:15 +0900

    기본틀짜기

---
+++ src/main/java/com/example/minimarket/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.example.minimarket.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+    @PostMapping ("/login")
+    public Map<String, String> test1() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "백엔드와의 연동 성공!");
+        return response;
+    }
+
+    @PostMapping ("/signup")
+    public Map<String, String> test2() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "백엔드와의 연동 성공!");
+        return response;
+    }
+
+}
---
+++ src/main/java/com/example/minimarket/service/OrderService.java
@@ -0,0 +1,4 @@
+package com.example.minimarket.service;
+
+public class OrderService {
+}
